24a3a3b6e18f47bb70bac15f2d5799c3d1fd0e7c
carolinapere2021 <117332687+carolinapere2021@users.noreply.github.com>
2022-11-11 14:46:53 -0500

    commit inicial
    commit

---
+++ js/main.js
@@ -0,0 +1,59 @@
+
+function Circulo(){
+    var figura = document.getElementById('figura');
+    figura.classList.toggle('Circulo');
+}
+
+function Rombo(){
+    var figura = document.getElementById('figura');
+    figura.classList.toggle('Rombo');
+}
+
+function Gif(){
+    var figura = document.getElementById('figura');
+    figura.classList.toggle('Gif');
+}
+
+function Triangulo() {
+    var figura= document.getElementById('figura');
+    figura.classList.toggle("Triangulo")}
+
+
+
+function arriba(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("top");
+}
+
+function izquierda(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("Left");
+}
+
+function abajo(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("Butom");
+}
+
+function derecha(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("Right");
+}
+
+function DiagonalIz(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("DiagonalIz");
+}
+function DiagonalDe(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("DiagonalDe");
+}
+
+function Huevo(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("Huevo");
+}
+function Semicirculo(){
+    var figura = document.getElementById('figura')
+    figura.classList.toggle("Semicirculo");
+}
